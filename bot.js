3867eb1b5e3393595f975904b0191bcf56756088
Jordan Zinger <jzinger18@gmail.com>
2017-08-24 20:09:44 -0400

    botkit setup and get active slack users

---
+++ bot.js
@@ -0,0 +1,66 @@
+//framework used for slackbot
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+	debug: false
+})
+
+
+//array used to store lust of team members 
+var fullTeamList = [];
+//array used to store users in a channel
+
+
+//connect the bot to stream of messages(slack rtm messaging api)
+controller.spawn({
+	token: 'xoxb-230959860769-Ne80biViSwKx6nyKUMKK37Mh'
+}).startRTM(function (err,bot) {
+	if(err) {
+		throw new Error(err)
+	}
+
+
+	bot.api.users.list({presence:true}, function(err, res) { 
+		if(res.hasOwnProperty('members') && res.ok) {
+			var total = res.members.length;
+			for(var i = 0; i < total; i++) {
+				var member = res.members[i]
+				if(member.presence === 'active') {
+				fullTeamList.push(' \n user: ' + member.name)
+				}
+			}
+			
+
+			// console.log(res)
+		}
+	
+		
+	})
+
+
+})
+
+
+
+
+	//bot will listen for the followint message
+	controller.hears('who is active',['direct_message','direct_mention', 'mention'], function(bot,message) {
+		bot.reply(message,'Here you got ' + 'this will be the list of active users ' + fullTeamList.join(''))
+
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
